de382f720a9aa5acba90b2d04bae479eb1c8520d
protocol114 <sorrowspear@gmail.com>
2014-07-16 15:33:26 -0600

    proper attempt at getting a UI going
    the last try was a fuck up. I realized some shit now

--- SAGD9/Assets/MoneyLabelScript.cs
+++ SAGD9/Assets/MoneyLabelScript.cs
@@ -3,11 +3,8 @@ using System.Collections;
 
 public class MoneyLabelScript : MonoBehaviour {
 
-    public GameObject GameDataObject;
-
 	// Use this for initialization
 	void Start () {
-        //this.guiText = GameDataObject.Money;
 	}
 	
 	// Update is called once per frame
